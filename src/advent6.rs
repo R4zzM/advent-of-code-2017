8cffcbbb550a0ead7f633c808a37a6d93e11f158
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2017-12-06 20:30:30 +0100

    A couple of solutions that at least look ok

--- src/advent6.rs
+++ src/advent6.rs
@@ -14,13 +14,11 @@ impl Configuration {
 }
 
 // Hash the bank configuration
-fn make_hash(arg: &[u32]) -> u64 {
-	let mut current = 0;
+fn make_hash(banks: &[u32]) -> u64 {
 	let mut s : String = String::new();
 	let mut hasher = DefaultHasher::new();
-    while current < arg.len() {
-    	s.push_str(arg[current].to_string().as_str());
-    	current += 1;
+	for block_count in banks.iter() {
+    	s.push_str((&block_count).to_string().as_str());
     }
 
 	s.hash(&mut hasher);
@@ -29,32 +27,29 @@ fn make_hash(arg: &[u32]) -> u64 {
 
 fn count_configurations(banks: &mut [u32]) -> (u64, u64) {
 	let mut configurations : Vec<Configuration> = Vec::new();
-	let config = Configuration::from(make_hash(banks), 0);
-	configurations.push(config);
+	let initial_config = Configuration::from(make_hash(banks), 0);
+	configurations.push(initial_config);
 
 	let mut loop_counter : u64 = 0; // Answer to first
 	let mut loop_size = 0; // Answer to second
-	let mut current = 0;
 	loop {
 		loop_counter += 1;
 
 		// Find bank with most blocks
-		current = 0;
 		let mut bank_top = 0;
 		let mut blocks_highest = 0;
-		while current < banks.len() {
-			if blocks_highest < banks[current] {
-				blocks_highest = banks[current];
-				bank_top = current;
+		for (i, block_count) in banks.iter().enumerate() {
+			if blocks_highest < *block_count {
+				blocks_highest = *block_count;
+				bank_top = i;
 			}
-			current += 1;
 		}
 
 		// Set the block count for the bank with most blocks to 0
 		banks[bank_top] = 0;
 
 		// Reallocate the blocks
-		current = bank_top + 1;
+		let mut current = bank_top + 1;
 		while blocks_highest > 0 {
 			if current == banks.len() {
 				current = 0;
@@ -68,7 +63,7 @@ fn count_configurations(banks: &mut [u32]) -> (u64, u64) {
 		let new_config = Configuration::from(make_hash(banks), loop_counter);
 
 		// Check if the initial configuration matches the current one, if it does, we are done.
-		match configurations.iter().find(|config| new_config.hash == config.hash) {
+		match configurations.iter().find(|config| config.hash == new_config.hash) {
 			None => (),
 			Some(config) => { 
 				loop_size = loop_counter - config.loop_size;
@@ -83,8 +78,8 @@ fn count_configurations(banks: &mut [u32]) -> (u64, u64) {
 }
 
 pub fn both() -> (u64, u64) {
-	let banks : &mut [u32; 16] = &mut [11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11];
-	let (counter, size) = count_configurations(banks);
+	let mut banks : [u32; 16] = [11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11];
+	let (counter, size) = count_configurations(&mut banks);
 
 	(counter, size)
 }
